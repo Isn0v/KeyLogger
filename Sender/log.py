7966be7dee0d8e68bc5c63dd3e138a2475ceb743
Isn0v <isnovikov0@gmail.com>
2024-02-17 17:38:09 +0700

    some proper sender changes

--- Sender/log.py
+++ Sender/log.py
@@ -18,7 +18,7 @@ def gmail_authenticate():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('Sendler/credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('Sender/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         with open("token.pickle", "wb") as token:
             pickle.dump(creds, token)
--- Sender/main.py
+++ Sender/main.py
@@ -1,10 +1,10 @@
 from time import sleep
-import Sendler.log as log
+import log as log
 from os import path, remove, listdir
 
 DELAY = 60
 MESSAGE = 'GUI'
-DIRECTORY = 'EmailsToSend'
+DIRECTORY = '../EmailsToSend'
 if __name__ == "__main__":
     while True:
         files = [file for file in listdir(DIRECTORY) if file.endswith('.txt')]
